381c4ce3b49acb69c916439a948fec81474283cf
Igor M <simplisticated.software@gmail.com>
2023-12-08 20:38:02 +0300

    Jest configuration

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    moduleFileExtensions: ['ts', 'js', 'json'],
+    preset: "ts-jest",
+    testEnvironment: "node",
+    moduleFileExtensions: ["ts", "js", "json"],
     transform: {
-        '^.+\\.ts$': 'ts-jest',
+        "^.+\\.ts$": "ts-jest",
     },
-    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.ts$',
-    setupFiles: ['dotenv/config'],
+    testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.ts$",
+    setupFiles: ["dotenv/config"],
 };
